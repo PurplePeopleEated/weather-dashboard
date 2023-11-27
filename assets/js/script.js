89cdcba87ac35beebe8fe62ba34294444e5ecbd4
Karra <karra.maynard@gmail.com>
2023-11-27 01:23:19 -0500

    add todos and api key/url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+// type city in search bar, 
 
+// display info about city
 
+// current and future conditions
+
+// city name, date, icon of weather, 
+// temp, humidity, wind speed
+
+// 5 day forecast
+
+// search history
+const API_Key = '3e0e9f7896f23481394fe11def7369f2';
+const API_URL = 'http://api.openweathermap.org/data/2.5/';
