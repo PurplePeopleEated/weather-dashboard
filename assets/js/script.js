d22c29a441fe0ba5884435f732d8f140b8d4f6f6
Karra <karra.maynard@gmail.com>
2023-11-28 01:27:45 -0500

    initial search history function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-// type city in search bar 
-
-// display info about city
-
-// current and future conditions
-
-// city name, date, icon of weather, 
-// temp, humidity, wind speed
-
-// 5 day forecast
-
-// search history
 const API_Key = '3e0e9f7896f23481394fe11def7369f2';
 const API_URL = 'http://api.openweathermap.org/data/2.5/';
 const API_GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
@@ -76,12 +64,23 @@ function displayWeather(city) {
     })
 }
 
+function history(city) {
+  sessionStorage.setItem('name', city);
+
+  let searchArea = document.querySelector('.search-area');
+  let btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.textContent = city;
+  searchArea.appendChild(btn);
+}
+
 // Allow stuff to happen when searchBtn is clicked
 let searchBtn = document.querySelector('.search-btn');
 
 searchBtn.addEventListener('click', function() {
   let city = cityEl.value.replace(/[^\D]/g, '');
   if (city != '') {
+    history(city);
     displayWeather(city);
   }
 });
